1d2bee4141bcd572ece3f6bd081b12ad1d1e56a0
Bob <bob@canthelpyou.com>
2015-08-17 18:38:56 +0200

    Add readme and pidgin-otr templates

---
+++ pidgin-otr/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'pidgin-otr'
+$url = 'https://otr.cypherpunks.ca/binaries/windows/pidgin-otr-4.0.1.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'exe'
+  url           = 'https://otr.cypherpunks.ca/binaries/windows/pidgin-otr-4.0.1.exe'
+
+  silentArgs = '/S'
+  
+  checksum      = '55ec19049273ea0a4ddd339aa84e10ae'
+  checksumType  = 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ pidgin-otr/tools/chocolateyuninstall.ps1
@@ -0,0 +1,28 @@
+$ErrorActionPreference = 'Stop';
+
+$packageName = 'pidgin-otr'
+$registryUninstallerKeyName = 'pidgin-otr'
+$shouldUninstall = $true
+
+$local_key     = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName"
+$local_key6432   = "HKCU:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName" 
+$machine_key   = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName"
+$machine_key6432 = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName"
+
+$file = @($local_key, $local_key6432, $machine_key, $machine_key6432) `
+    | ?{ Test-Path $_ } `
+    | Get-ItemProperty `
+    | Select-Object -ExpandProperty UninstallString
+
+if ($file -eq $null -or $file -eq '') {
+    Write-Host "$packageName has already been uninstalled by other means."
+    $shouldUninstall = $false
+}
+
+$installerType = 'EXE' 
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+if ($shouldUninstall) {
+ Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file
+}
